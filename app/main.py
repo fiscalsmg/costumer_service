8bc1bcf9f5ecc36bc0cf876423df9c606b08f7cd
Fiscal <fiscalguadalupe422@gmail.com>
2023-05-30 00:43:24 -0600

    feat: Se agrega configuracion para poder ejecutar aplicaciones ASGI en AWS lambda

--- app/main.py
+++ app/main.py
@@ -1,11 +1,59 @@
+import logging
+import sys
+
 from fastapi import FastAPI
 
-from app.entrypoint import app_client
+from mangum import Mangum
+
+
+from app.schemas.input_data_client import (
+    InputDataClient,
+    UpdateInputDataClient,
+)
+from app.config.customer_actions import ClientDAO
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logger = logging.getLogger(__name__)
+
 
 app: FastAPI = FastAPI(
-    title="Servicio para consultar clientes",
-    description="costumer service",
+    title="Ejercicio consula clientes",
+    description="Ejercicio 2 prueba practica",
     docs_url="/docs",
 )
 
-app.include_router(app_client)
+
+@app.get("/")
+def home():
+    return {"message": "Hola!"}
+
+
+# Obtener todos los clientes
+@app.get("/clients")
+def get_client():
+    costumers: ClientDAO = ClientDAO.selection()
+    return costumers
+
+
+# Crear un nuevo cliente
+@app.post("/save/clients")
+def crate_client(input: InputDataClient):
+    costumers: ClientDAO = ClientDAO.insertion(costumer=input)
+    return costumers
+
+
+# Actualizar los datos por numero de telefono
+@app.put("/client/{phone_number}")
+def update_client(phone_number: int, input: UpdateInputDataClient):
+    costumers: ClientDAO = ClientDAO.updated(costumer=input, id_phone=phone_number)
+    return costumers
+
+
+# Eliminar de la bd por numero de telefono
+@app.delete("/client/{phone_number}")
+def delete_cliente(phone_number: int):
+    costumers: ClientDAO = ClientDAO.delete(id_phone=phone_number)
+    return costumers
+
+
+handler = Mangum(app)
